37d9365c5330c2752a40b119f6a0229112288239
MoT186 <tommachefert78240@gmail.com>
2025-04-20 18:24:07 +0200

    Update map.c
    Map.c contient désormais seulement le contenu permettant de générer la map à partir des fichiers et la fonction demandant à l'utilisateur de choisir une map

--- map.c
+++ map.c
@@ -2,25 +2,66 @@
 #include <stdlib.h>
 #include "interface.h"
 
+int choix_map()
+{
+    int num_map=0;
+    char c;
+    do{
+        printf("Choisissez le numero de la carte (1-3): ");
+        if (scanf("%d", &num_map)!=1) 
+        {
+            printf("Valeur entree incorrect.\n");
+            vider_tampon();
+            num_map=0;
+        }
+        else {
+            if ((c = getchar()) != '\n') 
+            {
+                printf("Trop de valeur entree, reessayez.\n");
+                vider_tampon();
+                num_map = 0;
+            }
+            else if(num_map < 1 || num_map > 3) 
+            {
+                printf("Numsro de carte invalide. Veuillez choisir entre 1, 2 et 3.\n");
+                num_map=0;
+            }
+        }
 
-#define CARTE_TAILLE 20
+    } while (num_map < 1 || num_map > 3);
+    return num_map;   
+}
 
-int main()
+
+void generation_carte(int num,char **carte )
 {
 	//Création des tableaux de 20*20 pour les maps à partir des fichiers texte map.txt
 	char *ligne = NULL;
-	char **carte = NULL;
 	FILE* fichier = NULL;
-	
-	carte = malloc(CARTE_TAILLE*sizeof(char*));
 	if(carte == NULL)
 	{
 		printf("Erreur d'allocation memoire\n");
 		exit(1);
 	}
 	
+	//On ouvre la map que l'on va utiliser
+    switch (num)
+    {
+        case 1:
+            fichier = fopen("map1.txt","r");
+        break;
+        case 2:
+            fichier = fopen("map2.txt","r");
+        break;
+        case 3:
+            fichier = fopen("map3.txt","r");
+        break;
+        default:
+            printf("Erreur de choix de carte\n");
+            exit(1);
+            break;
+    } 
 	
-	fichier = fopen("map1.txt","r");
 	if(fichier == NULL)
 	{
 		printf("Ouverture du fichier impossible\n");
@@ -42,15 +83,11 @@ int main()
 			 
 		}
 		//Récupération du saut de ligne
-		char poubelle = fgetc(fichier);
+		fgetc(fichier);
 		carte[a]=ligne;
+
 	}
 	
 	affiche_tableau(carte,CARTE_TAILLE);
 	fclose(fichier);
-	for(int a=0;a<CARTE_TAILLE;a++)
-	{
-		free(carte[a]);
-	}
-	free(carte);
 }
