cf520c71d5f5fda93e4dc6feb23ef51ac541c770
MoT186 <tommachefert78240@gmail.com>
2025-04-20 18:25:34 +0200

    Update interface.h
    fichier contenant toutes les fonctions relatives à la gestion de la map et de l'interface

--- interface.h
+++ interface.h
@@ -1,6 +1,9 @@
 #ifndef INTERFACE_H
 	#define INTERFACE_H
-	
-	void affiche_tableau(char** tab,int taille);
+    	#define CARTE_TAILLE 20
+    	void affiche_tableau(char** tab,int taille);
+    	void generation_carte(int num,char **carte);
+    	void vider_tampon();
+   	int choix_map();
 	
 #endif
