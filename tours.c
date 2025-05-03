e7fa7f3b2b500397ef99fbb98ecc81f752b75128
xFireANTx <anton.schaff@gmail.com>
2025-05-03 13:03:15 +0200

    Update tours.c

--- tours.c
+++ tours.c
@@ -49,55 +49,7 @@ void tours(Combattant *equipe1, Combattant *equipe2, char **carte){
     }
 }
 
-int verif_coord(int* x, int* y,int xMax, int yMax, int xMin, int yMin) { 
-    // x et y sont les coordonnees du joueur    
-    printf("Entrez x entre %d et %d : ", xMin, xMax);
-    scan_int(x);//Regarder le fichier utilitaire pour voir comment fonctionne scan_int
-    if (*x < xMin || *x> xMax) {                     //On regarde si les coordonees sont dans l intervalle de deplacement
-        printf("x=%d n'est pas accessible.\n",*x);
-        return 0;
-    }
 
-    printf("Entrez y entre %d et %d : ", yMin, yMax);
-    scan_int(y);//fichier utilitaire
-    if (*y < yMin || *y > yMax) {
-        printf("y=%d n'est pas accessible.\n",*y);
-        return 0;
-    }
-
-    printf("Pas de probleme.\n");
-    printf("Nouvelle position : x = %d, y = %d\n", *x, *y);
-    return 1;
-}
-void deplacement(Combattant *perso,char **map){   
-    int x,y,portee;
-    x = perso->position_x;
-    y = perso->position_y;
-    portee = perso->deplacement;
-
-    //cases disponible pour mouvement
-    int xMin = x-portee ,yMin = y-portee ;        
-    int xMax = x+portee ,yMax = y+portee;
-   //on fait en sorte que les Min et Max ne sortent pas de la map
-    if(x-portee<0){                          
-        xMin = 0;
-    }
-    if(x+portee>CARTE_TAILLE-1){ 
-        xMax = CARTE_TAILLE-1;
-    }
-    if(y-portee<0){
-        yMin=0;
-    }
-    if(y+portee>CARTE_TAILLE-1){
-        yMax=CARTE_TAILLE-1;
-    }
-
-    int t = 0;
-    while(!t){
-        t = verif_coord(perso->position_x,perso->position_y,xMax, yMax, xMin, yMin);
-    }
-    return;
-}
 
 int verifATT(Combattant* perso, int i){
     int x,y =0;
